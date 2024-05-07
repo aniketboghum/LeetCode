968c1ab3a2caa0f8b25e2bf6d86bf997140f1279
aniketboghum <boghumaniket@gmail.com>
2024-05-07 17:41:31 +0530

    made Addtwo number code look cleaner

--- AddTwoNumber_medium.java
+++ AddTwoNumber_medium.java
@@ -1,13 +1,5 @@
 package LeetCode;
 
-//class ListNode {
-//      int val;
-//      ListNode next;
-//      ListNode() {}
-//      ListNode(int val) { this.val = val; }
-//      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-//}
-
 public class AddTwoNumber_medium {
     public  static  ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode add;
